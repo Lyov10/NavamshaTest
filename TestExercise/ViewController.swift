d8aa6908489c4ab8f1ed50850d7912dacb04a529
Levon Mnatsakanyan <mnatsakanyanlevon3@gmail.com>
2024-02-04 12:32:06 +0400

    Initial Commit

---
+++ TestExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestExercise
+//
+//  Created by 4steps on 04.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
